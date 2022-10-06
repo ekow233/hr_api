9970419bc3fdd210cc610ae6c8fb319480c41562
unionsghr <hr@unionsg.com>
2022-10-06 09:57:30 +0000

    User Registration
    User registration has been added, roles can now be assigned to users during registration, user api registration routes are also in place

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -2,24 +2,63 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Spatie\Permission\Models\Role;
 
 class RegisterController extends Controller
 {
     //Register function to take care of user registration from api
     public function register(Request $request){
 
+        // return("creating user");
+
         //validate user creation details
-        $validator = Validator::make($request->all(), [
+        // $validator = Validator::make($request->all(), [
+        //     'username' => 'required',
+        //     'email' => 'required|email',
+        //     'password' => 'required',
+        //     'employee' => 'required'
+        // ]);
+
+        // $validator = $request->validate([
+        //     'username' => 'required',
+        //     'email' => 'required|email|unique:users,email',
+        //     'password' => 'required',
+        //     'employee' => 'required|unique:users,employee',
+        //     'role_id' => 'required'
+        // ]);
+
+        $validator = Validator::make($request->all(),[
             'username' => 'required',
-            'email' => 'required|email',
-            'password' => 'required'
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+            'employee' => 'required|unique:users,employee',
+            'role_id' => 'required'
         ]);
 
+        //if input data fails return error message to the user
         if($validator->fails()){
-            return response()->json(['response_code'=>'401','message'=>'user details validation failed']);
+            $error_msg = $validator->messages();
+            return response()->json(['response_code'=>'401','message'=>'user details validation failed '.$error_msg]);
         }else{
+
+            //get all request
+            $input = $request->all();
+
+            //decrypt the password
+            $input['password'] = bcrypt($input['password']);
+
+            //now create the user
+            $user = User::create($input);
+
+            //assign role to user
+            // $admin = Role::create(['name' => 'Admin3']);
+            $role = Role::find($request->role_id);
+            $user->assignRole([$role]);
+
             return response()->json(['response_code'=>'200','message'=>'user created successfully']);
         }
     }
