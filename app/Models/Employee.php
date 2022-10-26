499ad15ef9e0110502329a79be1c9d9a5302dd8f
unionsghr <hr@unionsg.com>
2022-10-26 14:04:58 +0000

    Employee controller
    Adding employee has been modified and made efficient

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -10,9 +10,28 @@ class Employee extends Model
     use HasFactory;
 
     protected $fillable = [
+            'employee_id',
+            'title', 
+            'first_name', 
+            'middle_name',
+            'last_name',
+            'birthday',
+            'bank_acc_no',
+            'pay_grade',
+            'notches',
+            'home_phone',
+            'mobile_phone',
+            'work_phone',
+            'work_email',
+            'private_email',
+            'recruitment_date',
+            'supervisor',
+            'indirect_supervisors',
+            'branch'
     ];
 
     protected $casts = [
-        'indirect_supervisors' => 'array'
+
+        // 'indirect_supervisors' => 'array'
     ];
 }
